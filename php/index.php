4ddce05e5ccbf3e6d83963d5fc663a913a2ff48b
Deathur <matthieu.baelen@gmail.com>
2025-06-30 10:40:14 +0200

    test docker php

--- php/index.php
+++ php/index.php
@@ -5,28 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <?php 
-    if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
-    echo 'Vous utilisez Firefox.';
-    }
-    if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
-    echo 'Vous utilisez Google Chrome.';
-    }
-    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE ||
-    strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
-    echo 'You are using Internet Explorer.<br />';
-    }
-    ?>
-    <form action="action.php" method="post">
-        <label>Votre nom :</label>
-        <input name="nom" id="nom" type="text" />
+    <body>
+        <?php 
+        if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+        echo 'Vous utilisez Firefox.';
+        }
+        if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Chrome')) {
+        echo 'Vous utilisez Google Chrome.';
+        }
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE ||
+        strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
+        echo 'You are using Internet Explorer.<br />';
+        }
+        ?>
+        <form action="action.php" method="post">
+            <label>Votre nom :</label>
+            <input name="nom" id="nom" type="text" />
 
-        <label>Votre âge :</label>
-        <input name="age" id="age" type="number" /></p>
+            <label>Votre âge :</label>
+            <input name="age" id="age" type="number" /></p>
 
-        <button type="submit">Valider</button>
-    </form>
-    
-</body>
+            <button type="submit">Valider</button>
+        </form>
+        
+    </body>
 </html>
